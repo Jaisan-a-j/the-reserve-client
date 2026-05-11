ae6903de8e7d8643735e3c0e1e7e6f03ffcb90d8
jaisan <jaisanaj1999@gmail.com>
2026-05-11 13:18:23 +0530

    Finished hamberger menu ui and functionality

---
+++ src/constants/menus.ts
@@ -0,0 +1,9 @@
+import type { LinkTypes } from "../types";
+
+export const navLinks: LinkTypes[] = [
+  { name: "Home", active: true },
+  { name: "About Us", active: false },
+  { name: "Services", active: false },
+  { name: "Team", active: false },
+  { name: "FAQS", active: false },
+];
