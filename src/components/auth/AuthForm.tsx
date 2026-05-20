b55b8eec5fcd7ae8656e0f018c338d55beb4b6df
jaisan <jaisanaj1999@gmail.com>
2026-05-20 20:40:25 +0530

    fixed empty field error message bugs

--- src/components/auth/AuthForm.tsx
+++ src/components/auth/AuthForm.tsx
@@ -37,16 +37,24 @@ const AuthForm = () => {
 
     setErrorMessage("");
 
-    if (!formData.email || !formData.password) {
-      setErrorMessage("Email and password are required");
-      return;
+    const errors = [];
+
+    if (!userExist && !formData.fullName.trim()) {
+      errors.push("Full name");
     }
 
-    if (!userExist && !formData.fullName) {
-      setErrorMessage("Full name is required");
-      return;
+    if (!formData.email.trim()) {
+      errors.push("Email");
     }
 
+    if (!formData.password.trim()) {
+      errors.push("Password");
+    }
+
+    if (errors.length > 0) {
+      setErrorMessage(`${errors.join(", ")} is required`);
+      return;
+    }
     try {
       if (userExist) {
         await dispatch(
@@ -77,6 +85,16 @@ const AuthForm = () => {
         setErrorMessage("Something went wrong");
       }
     }
+  };
+
+  const changeForm = () => {
+    setErrorMessage("");
+    setFormData({
+      fullName: "",
+      email: "",
+      password: "",
+    });
+    setUserExist(!userExist);
   };
 
   return (
@@ -111,6 +129,16 @@ const AuthForm = () => {
           onChange={handleChange}
           name="password"
         />
+
+        {userExist && (
+          <a
+            href="#"
+            className="text-sm font-semibold text-[#7c5dfa] hover:underline flex justify-end"
+          >
+            Forgot Password?
+          </a>
+        )}
+
         {errorMessage && (
           <p className="text-red-500 text-sm font-medium">{errorMessage}</p>
         )}
@@ -124,7 +152,7 @@ const AuthForm = () => {
         {!userExist ? "Already have an account?" : "Don't have an account?"}{" "}
         <a
           className="text-[#7c5dfa] font-bold hover:underline cursor-pointer"
-          onClick={() => setUserExist(!userExist)}
+          onClick={changeForm}
         >
           {!userExist ? "Login" : " Register"}
         </a>
